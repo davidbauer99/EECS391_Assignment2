f49e2bc25fb65433d1867eaadefc629bc604f625
David Bauer <dmb172@case.edu>
2016-02-29 13:43:22 -0500

    Implemented generation of child states. Range detection may need to fixed based on actual calculation of ranges in the game.
    Signed-off-by: David Bauer <dmb172@case.edu>

--- src/edu/cwru/sepia/agent/minimax/GameState.java
+++ src/edu/cwru/sepia/agent/minimax/GameState.java
@@ -5,10 +5,16 @@ import edu.cwru.sepia.action.ActionType;
 import edu.cwru.sepia.action.DirectedAction;
 import edu.cwru.sepia.action.TargetedAction;
 import edu.cwru.sepia.environment.model.state.State;
-import edu.cwru.sepia.environment.model.state.Unit;
+import edu.cwru.sepia.environment.model.state.ResourceNode.ResourceView;
+import edu.cwru.sepia.environment.model.state.Unit.UnitView;
+import edu.cwru.sepia.environment.model.state.UnitTemplate.UnitTemplateView;
+import edu.cwru.sepia.model.GameUnitTest;
 import edu.cwru.sepia.util.Direction;
 
+import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.util.*;
+import java.util.Map.Entry;
 
 /**
  * This class stores all of the information the agent
@@ -19,7 +25,23 @@ import java.util.*;
  * but do not delete or change the signatures of the provided methods.
  */
 public class GameState {
+	
+	//Create a set of just N,S,E,W for iteration.
+	private static final Set<Direction> VALID_DIRECTIONS = new HashSet<Direction>();
+	{
+		VALID_DIRECTIONS.add(Direction.NORTH);
+		VALID_DIRECTIONS.add(Direction.WEST);
+		VALID_DIRECTIONS.add(Direction.EAST);
+		VALID_DIRECTIONS.add(Direction.SOUTH);
+	}
 
+	private final int xExtent;
+	private final int yExtent;
+	private final List<ResourceView> resources;
+	private final List<UnitState> footmen;
+	private final List<UnitState> archers;
+	private final boolean isFootmanTurn;
+	
     /**
      * You will implement this constructor. It will
      * extract all of the needed state information from the built in
@@ -42,9 +64,41 @@ public class GameState {
      * @param state Current state of the episode
      */
     public GameState(State.StateView state) {
+    	this.xExtent = state.getXExtent();
+    	this.yExtent = state.getYExtent();
+    	this.resources = state.getAllResourceNodes();
+    	this.footmen = createUnitStates(state.getUnits(0));
+    	this.archers = createUnitStates(state.getUnits(1));
+    	this.isFootmanTurn = true;
+    }
+    
+    public GameState(int xBound, int yBound, List<UnitState> foots,
+			List<UnitState> archs, List<ResourceView> resourceList, boolean isFootmanTurn) {
+		this.xExtent = xBound;
+		this.yExtent = yBound;
+		this.footmen = foots;
+		this.archers = archs;
+		this.resources = resourceList;
+		this.isFootmanTurn = isFootmanTurn;
+	}
+
+	public boolean isTerminal() {
+    	return archers.isEmpty() || footmen.isEmpty();
     }
 
-    /**
+    private List<UnitState> createUnitStates(List<UnitView> units) {
+		List<UnitState> unitStates = new ArrayList<UnitState>();
+		for (UnitView view : units) {
+			UnitTemplateView unitTemplate = view.getTemplateView();
+			unitStates.add(new UnitState(view.getXPosition(),
+					view.getYPosition(), view.getHP(), 
+					unitTemplate.getBasicAttack() + unitTemplate.getPiercingAttack(),
+					unitTemplate.getRange(), view.getID()));
+		}
+		return unitStates;
+	}
+
+	/**
      * You will implement this function.
      *
      * You should use weighted linear combination of features.
@@ -88,19 +142,154 @@ public class GameState {
      * @return All possible actions and their associated resulting game state
      */
     public List<GameStateChild> getChildren() {
-    	/*
-    	 * If footman turn:
-    	 * 	For each footman:
-    	 * 		All valid moves (check obstacles and map boundaries)
-    	 * 		if distance to archer is <= 1 add attack
-    	 * 	Create game state for each pair of actions, apply changes
-    	 * 
-    	 * If Archer turn:
-    	 * 	For each archer:
-    	 * 		All valid moves
-    	 * 		Attack
-    	 * 	Create game state for each pair of actions, apply changes
-    	 */
-        return null;
+    	Map<Integer, List<Action>> actions = new HashMap<Integer, List<Action>>();
+    	if (isFootmanTurn) {
+    		for (UnitState footman : footmen) {
+    			List<Action> footmanActions = new ArrayList<Action>();
+    			footmanActions.addAll(moveActions(footman));
+    			footmanActions.addAll(attackActions(footman, archers, footman.getRange()));
+    			actions.put(footman.getId(), footmanActions);
+    		}
+    	} else {
+    		for (UnitState archer : archers) {
+    			List<Action> archerActions = new ArrayList<Action>();
+    			archerActions.addAll(moveActions(archer));
+    			archerActions.addAll(attackActions(archer, footmen, archer.getRange()));
+    			actions.put(archer.getId(), archerActions);
+    		}
+    	}
+    	List<GameStateChild> childrenStates = generateChildren(actions);
+        return childrenStates;
     }
+
+	private List<GameStateChild> generateChildren(Map<Integer, List<Action>> actions) {
+		List<Map<Integer, Action>> actionPairings = getCrossProductOfActions(actions);
+		List<GameStateChild> children = new ArrayList<GameStateChild>();
+		for (Map<Integer, Action> actionPair : actionPairings) {
+			children.add(childState(this, actionPair));
+		}
+		return children;
+	}
+
+	private GameStateChild childState(GameState gameState,
+			Map<Integer, Action> actions) {
+		int xBound = gameState.xExtent;
+		int yBound = gameState.yExtent;
+		List<ResourceView> resourceList = gameState.resources;
+		List<UnitState> foots = new ArrayList<UnitState>();
+		for (UnitState unit : gameState.footmen) {
+			foots.add(new UnitState(unit.getXPos(),
+					unit.getYPos(), unit.getHealth(),
+					unit.getDamage(), unit.getRange(), unit.getId()));
+		}
+		List<UnitState> archs = new ArrayList<UnitState>();
+		for (UnitState unit : gameState.archers) {
+			archs.add(new UnitState(unit.getXPos(),
+					unit.getYPos(), unit.getHealth(),
+					unit.getDamage(), unit.getRange(), unit.getId()));
+		}
+		if (gameState.isFootmanTurn) {
+			applyActions(foots, archs, actions);
+		} else {
+			applyActions(archs, foots, actions);
+		}
+		GameState newState = new GameState(xBound, yBound, foots,
+				archs, resourceList, !gameState.isFootmanTurn);
+		return new GameStateChild(actions, newState);
+	}
+
+	private void applyActions(List<UnitState> units, List<UnitState> targets,
+			Map<Integer, Action> actions) {
+		for (Action action : actions.values()) {
+			if (action.getType() == ActionType.PRIMITIVEMOVE) {
+				UnitState unit = unitByID(units, action.getUnitId());
+				DirectedAction dirAction = (DirectedAction) action;
+				unit.setXPos(unit.getXPos() + dirAction.getDirection().xComponent());
+				unit.setYPos(unit.getYPos() + dirAction.getDirection().yComponent());
+			} else {
+				TargetedAction targAction = (TargetedAction) action;
+				UnitState unit = unitByID(units, targAction.getUnitId());
+				UnitState target = unitByID(targets, targAction.getTargetId());
+				target.setHealth(target.getHealth() - unit.getDamage());
+			}
+		}
+	}
+
+	private UnitState unitByID(List<UnitState> units, int id) {
+		for (UnitState u : units) {
+			if (u.getId() == id) {
+				return u;
+			}
+		}
+		return null;
+	}
+
+	private List<Map<Integer, Action>> getCrossProductOfActions(
+			Map<Integer, List<Action>> actions) {
+		Iterator<Entry<Integer, List<Action>>> actionEntries = actions.entrySet().iterator();
+		
+		List<Map<Integer, Action>> actionPairings = new ArrayList<Map<Integer,Action>>();
+		actionPairings.addAll(firstUnitsActions(actionEntries.next()));
+		List<Map<Integer, Action>> existingPairings = actionPairings;
+		
+		Entry<Integer, List<Action>> nextActions = actionEntries.next();
+		while (nextActions != null) {
+			actionPairings = new ArrayList<Map<Integer, Action>>();
+			for (Map<Integer, Action> actionMap : existingPairings) {
+				for (Action nextAction : nextActions.getValue()) {
+					Map<Integer, Action> newActionMap = new HashMap<Integer, Action>(actionMap);
+					newActionMap.put(nextActions.getKey(), nextAction);
+					actionPairings.add(newActionMap);
+				}
+			}
+			existingPairings = actionPairings;
+		}
+		return actionPairings;
+	}
+
+	private Collection<? extends Map<Integer, Action>> firstUnitsActions(
+			Entry<Integer, List<Action>> actionEntry) {
+		List<Map<Integer, Action>> actionList = new ArrayList<Map<Integer,Action>>();
+		for (Action action : actionEntry.getValue()) {
+			Map<Integer, Action> actionMap = new HashMap<Integer, Action>();
+			actionMap.put(actionEntry.getKey(), action);
+			actionList.add(actionMap);
+		}
+		return actionList;
+	}
+
+	private List<Action> attackActions(UnitState unit, List<UnitState> targets, int range) {
+		List<Action> attacks = new ArrayList<Action>();
+		for (UnitState target : targets) {
+			if (distance(unit, target) <= range) {
+				attacks.add(Action.createPrimitiveAttack(unit.getId(), target.getId()));
+			}
+		}
+		return attacks;
+	}
+
+	private double distance(UnitState unitA, UnitState unitB) {
+		Point2D pointA = new Point2D.Double(unitA.getXPos(), unitA.getYPos());
+		Point2D pointB = new Point2D.Double(unitB.getXPos(), unitB.getYPos());
+		return Math.abs(pointA.distance(pointB));
+	}
+
+	private List<Action> moveActions(UnitState unit) {
+		List<Action> moveActions = new ArrayList<Action>();
+		int x = unit.getXPos();
+		int y = unit.getYPos();
+		int id = unit.getId();
+		for (Direction direction : VALID_DIRECTIONS) {
+			int newX = x + direction.xComponent();
+			int newY = y + direction.yComponent();
+			if (isValidSpace(newX, newY)) {
+				moveActions.add(Action.createPrimitiveMove(id, direction));
+			}
+		}
+		return moveActions;
+	}
+
+	private boolean isValidSpace(int newX, int newY) {
+		return 0 <= newX && 0 <= newY && newX < xExtent && newY < yExtent;
+	}
 }
---
+++ src/edu/cwru/sepia/agent/minimax/UnitState.java
@@ -0,0 +1,70 @@
+package edu.cwru.sepia.agent.minimax;
+
+public class UnitState {
+
+	private int xPos;
+	private int yPos;
+	private int health;
+	private int damage;
+	private int range;
+	private int id;
+	
+	public UnitState(int xPos, int yPos, int health, int damage, int range, int id) {
+		super();
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.health = health;
+		this.damage = damage;
+		this.range = range;
+		this.id = id;
+	}
+
+	public int getXPos() {
+		return xPos;
+	}
+
+	public void setXPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
+	public void setYPos(int yPos) {
+		this.yPos = yPos;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+	
+}
